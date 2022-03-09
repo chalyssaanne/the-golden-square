54a0837d23f35396c14f12f2afde87f9d59cc671
Chalyssa <chalyssa.pasay@gmail.com>
2022-03-09 11:21:33 +0000

    New commit

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,22 +1,9 @@
 require 'counter'
 
 RSpec.describe Counter do
-  it "initially reports 0" do
+  it "counts from 0 onwards" do
     counter = Counter.new
-    expect(counter.report).to eq "Counted to 0 so far." 
+    counter.add(5)
+    expect(counter.report).to eq "Counted to 5 so far." 
   end
-
-	it "reports a count of added values with each addition" do
-	  counter = Counter.new
-		counter.add(7)
-	  expect(counter.report).to eq "Counted to 7 so far." 
-	end
-
-	it "reports a count of all added values" do
-	  counter = Counter.new
-		counter.add(7)
-		counter.add(5)
-		counter.add(6)
-	  expect(counter.report).to eq "Counted to 18 so far." 
-	end
 end
